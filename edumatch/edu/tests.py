b6f95c127df88c2da4745960d0e31a828c3cdfde
Pattaranan Srisara <pattaranan2000@gmail.com>
2020-01-12 21:19:07 +0700

    refactor view to use template

--- edumatch/edu/tests.py
+++ edumatch/edu/tests.py
@@ -9,3 +9,7 @@ class HomepageTest(TestCase):
     def test_root_resolve_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func,home_page)
+    
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
