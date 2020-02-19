1d21721854a4b26baf903295e9e9932e44c7b2e8
ArtJr <45701958+ArtJr@users.noreply.github.com>
2020-02-20 01:36:31 +0700

    Create a unitTest for sent and cancel request with authUser

--- edumatch/edu/tests.py
+++ edumatch/edu/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from edu.views import home_page
 from edu.views import register
+from edu.views import match_result
 from edu.models import Tutor,Matched_Request
 from django.contrib.auth.models import User
 
@@ -55,7 +56,7 @@ class RegisterTest(TestCase):
         self.assertEqual(found.func,register)
 
     
-    def test_rendering_homepageTemplate(self):
+    def test_rendering_registerTemplate(self):
         response = self.client.get('/register')
         self.assertTemplateUsed(response, 'register.html')
 
@@ -78,3 +79,65 @@ class RegisterTest(TestCase):
         self.assertEqual(first_saved_tutor.gender,second_saved_tutor.gender)
         self.assertEqual(first_saved_tutor.city,second_saved_tutor.city)
         self.assertNotEqual(first_saved_tutor.expert,second_saved_tutor.expert)
+
+class TutorRequestTest(TestCase):
+
+    def test_URL_maping_to_matchResultVIEW(self):
+        found = resolve('/match-result/')
+        self.assertEqual(found.func,match_result)
+
+    
+    def test_rendering_matchResultTemplate(self):
+        frankin_user = User.objects.create_user('frankin','frankin@test.com','frankinpassword')
+        frankin = Tutor.objects.create(user=frankin_user,name='Frankin',gender = 'Male',city = 'Bangkok',expert ='Statistic')
+        self.client.login(username='frankin', password='frankinpassword') 
+        response = self.client.get('/match-result/')
+        self.assertTemplateUsed(response, 'manage_match.html')
+
+    def test_sent_and_recieve_request(self):
+        frankin_user = User.objects.create_user('frankin','frankin@test.com','frankinpassword')
+        ronnie_user = User.objects.create_user('ronnie','ronnie@test.com','ronniepassword')
+        betty_user = User.objects.create_user('betty','betty@test.com','bettypassword')
+        frankin = Tutor.objects.create(user=frankin_user,name='Frankin',gender = 'Male',city = 'Bangkok',expert ='Statistic')
+        ronnie = Tutor.objects.create(user=ronnie_user,name='Ronnie',gender = 'Male',city = 'Bangkok',expert ='Signal')
+        betty = Tutor.objects.create(user=betty_user,name='Betty',gender = 'Female',city = 'Bangkok',expert ='Signal')
+        self.client.login(username='frankin', password='frankinpassword') 
+        self.client.get(f'/match-request/send/{ronnie.id}')
+        self.client.get(f'/match-request/send/{betty.id}')
+
+        saved_requests = Matched_Request.objects.all()
+        self.assertEqual(saved_requests.count(),2)
+
+        first_saved_request = saved_requests[0]
+        second_saved_request = saved_requests[1]
+
+        self.assertEqual(first_saved_request.to_user.username,'ronnie')
+        self.assertEqual(first_saved_request.from_user.username,'frankin')
+        self.assertEqual(second_saved_request.to_user.username,'betty')
+        self.assertEqual(second_saved_request.from_user.username,'frankin')
+
+    def test_recieve_and_cancel_request(self):
+        frankin_user = User.objects.create_user('frankin','frankin@test.com','frankinpassword')
+        ronnie_user = User.objects.create_user('ronnie','ronnie@test.com','ronniepassword')
+        betty_user = User.objects.create_user('betty','betty@test.com','bettypassword')
+        frankin = Tutor.objects.create(user=frankin_user,name='Frankin',gender = 'Male',city = 'Bangkok',expert ='Statistic')
+        ronnie = Tutor.objects.create(user=ronnie_user,name='Ronnie',gender = 'Male',city = 'Bangkok',expert ='Signal')
+        betty = Tutor.objects.create(user=betty_user,name='Betty',gender = 'Female',city = 'Bangkok',expert ='Signal')
+
+        Matched_Request.objects.create(to_user=ronnie_user,from_user=frankin_user)
+        Matched_Request.objects.create(to_user=betty_user,from_user=frankin_user)
+        saved_requests = Matched_Request.objects.all()
+        self.assertEqual(saved_requests.count(),2)
+
+        self.client.login(username='frankin', password='frankinpassword') 
+        self.client.get(f'/match-request/cancel/{ronnie.id}')
+
+        saved_requests = Matched_Request.objects.all()
+        self.assertEqual(saved_requests.count(),1)
+
+        first_saved_request = saved_requests[0]
+
+        self.assertEqual(first_saved_request.to_user.username,'betty')
+        self.assertEqual(first_saved_request.from_user.username,'frankin')
+        
+
