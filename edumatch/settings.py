de11ae81acf2a94f065b847a5443fba9b3b6db05
pattaranan <pattaranan2000@gmail.com>
2020-03-24 00:01:06 +0700

    config settings.py (7)

--- edumatch/settings.py
+++ edumatch/settings.py
@@ -149,18 +149,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 # The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
