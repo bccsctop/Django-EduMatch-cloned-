0ff5db908e49910541ec541feda9a676bbd23bcf
pattaranan <pattaranan2000@gmail.com>
2020-03-23 19:55:29 +0700

    config settings.py (ถ)

--- edumatch/asgi.py
+++ edumatch/asgi.py
@@ -10,9 +10,9 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 import os
 import django
 from channels.layers import get_channel_layer
-from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edumatch.settings')
 django.setup()
 channel_layer = get_channel_layer()
-application = get_asgi_application()
+application = get_default_application()
