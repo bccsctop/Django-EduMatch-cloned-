f0f1839a279ec5bb5528e0263ee22087a1af9bdf
Siri Kijjaboriboon <s6101012620164@email.kmutnb.ac.th>
2020-03-25 16:54:34 +0700

    Add unittest for chat app

--- chat/tests.py
+++ chat/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from chat.views import room
+from django.urls import resolve
 
-# Create your tests here.
+
+
+class ChatTests(TestCase):
+    
+
+    def test_rootURL_mapping_to_ChatRoom(self):
+        
+        found = resolve('/chat/betty.frankin/')
+        self.assertEqual(found.func,room)
+
+    def test_rendering_chatTemplate(self):
+
+        #Login as Frankin (Require User Authentication)
+        self.client.login(username='frankin', password='frankinpassword') 
+
+        #Check template used ,user that loged in and room name when access chat room
+        response = self.client.get('/chat/betty.frankin/')
+        self.assertTemplateUsed(response, 'chat/room.html')
+        self.assertContains(response,'frankin') #user log in now
+        self.assertContains(response,'betty.frankin') #room name
