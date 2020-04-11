ef3af5f6a03b7c27978b84917ade0f8f03cf5698
Siri Kijjaboriboon <s6101012620164@email.kmutnb.ac.th>
2020-04-12 01:04:52 +0700

    Add comment to cacel_match_request in views.py

--- edu/views.py
+++ edu/views.py
@@ -140,6 +140,11 @@ def send_match_request(request, tutor_id):
 
 
 def cancel_match_request(request, tutor_id):
+    """ In cancel_match_request, this method will active when user authenticated.
+    we will get user'object which is the id of user that you want to cancel request
+    and filter the Match_Request by your username and username of user that you send 
+    the request and delete the request.
+    """
     if request.user.is_authenticated:
         user = get_object_or_404(User, id=tutor_id)
         frequest = Matched_Request.objects.filter(
