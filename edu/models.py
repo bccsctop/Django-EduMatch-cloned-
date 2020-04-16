247e68cbff2d9139f2f7400d3eee6ecabf6b1b47
Siri Kijjaboriboon <s6101012620164@email.kmutnb.ac.th>
2020-04-16 22:09:46 +0700

    Add comments to MatchRequest and sub func in models.py

--- edu/models.py
+++ edu/models.py
@@ -26,10 +26,16 @@ class Tutor(models.Model):
     
     
 class MatchedRequest(models.Model):
+    #In MatchRequest , it is for store request.
+    #When each user send the request to another user for being a tutor
+    #to_user = this request send to who
     to_user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='to_user')
+    #from_user = who send this request
     from_user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='from_user')
+    #Fetch time
     timestamp = models.DateTimeField(auto_now_add=True)
     
+    #when you in admin site you can see each object with this format "From (from_user), to (to_user)" by this function
     def __str__(self):
         return "From {}, to {}".format(self.from_user.username, self.to_user.username)
 
