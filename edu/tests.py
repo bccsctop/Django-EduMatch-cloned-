844083e5b409bb677c30e5c0d6d7d76cc7625ca8
Siri Kijjaboriboon <s6101012620164@email.kmutnb.ac.th>
2020-04-29 17:38:52 +0700

    Add comment to remove review unit test

--- edu/tests.py
+++ edu/tests.py
@@ -360,6 +360,7 @@ class ReviewTest(TestCase):
         self.assertContains(response,'ronnie is the best')
 
     def test_remove_review(self):
+        #create user
         frankin_user = User.objects.create_user(username='frankin',email='frankin@test.com',password='frankinpassword',first_name='Frankin',last_name='Alibaba')
         ronnie_user = User.objects.create_user(username='ronnie',email='ronnie@test.com',password='ronniepassword',first_name='Ronnie',last_name='Bacham')
         betty_user = User.objects.create_user(username='betty',email='betty@test.com',password='bettypassword',first_name='Betty',last_name='Caesar')
@@ -367,31 +368,33 @@ class ReviewTest(TestCase):
         frankin = UserAccount.objects.create(user=frankin_user,name='Frankin',gender = 'Male',city = 'Bangkok',expert ='Statistic')
         ronnie = UserAccount.objects.create(user=ronnie_user,name='Ronnie',gender = 'Male',city = 'Bangkok',expert ='Signal')
         betty = UserAccount.objects.create(user=betty_user,name='Betty',gender = 'Female',city = 'Bangkok',expert ='Signal')
-
+        #create review to ronnie
         Review.objects.create(comment='Ronnie is very good tutor',reviewer=frankin, reviewed_tutor=ronnie, rate=4)
         Review.objects.create(comment='Ronnie is a best tutor',reviewer=betty, reviewed_tutor=ronnie, rate=5)
-
+        #login as frankin
         self.client.login(username='frankin', password='frankinpassword')
+        #get in to review page
         response = self.client.get(f'/review/{ronnie.id}')
         self.assertTemplateUsed(response, 'review.html')
-
+        #check review that are display on template
         self.assertContains(response,'Frankin')
         self.assertContains(response,'Ronnie is very good tutor')
         self.assertContains(response,'Remove')
         self.assertContains(response,'Betty')
         self.assertContains(response,'Ronnie is a best tutor')
-
+        #remove frankin review
         review = Review.objects.get(reviewer=frankin)
         response = self.client.get(reverse('remove_review',args=[ronnie.id,review.id]), follow=True)
-
+        #check frankin review has been removed
         self.assertNotContains(response,'Frankin')
         self.assertNotContains(response,'Ronnie is very good tutor')
         self.assertNotContains(response,'Remove')
         self.assertContains(response,'Betty')
         self.assertContains(response,'Ronnie is a best tutor')
-
+        #still login as frankin
+        #remove other review by sending url
         review = Review.objects.get(reviewer=betty)
         response = self.client.get(reverse('remove_review',args=[ronnie.id,review.id]), follow=True)
-
+        #review still display on template
         self.assertContains(response,'Betty')
         self.assertContains(response,'Ronnie is a best tutor')
